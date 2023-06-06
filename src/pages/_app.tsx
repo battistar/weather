6d1cd316f0b5547a73328486299a0db00d883b27
Samuele Battistella <battistar92@gmail.com>
2023-06-06 16:40:56 +0200

    Persist Redux store on page change

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,10 +3,11 @@ import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 import { CssBaseline, ThemeProvider, createTheme, useMediaQuery } from '@mui/material';
-import store from 'store/store';
+import store, { persistor } from 'store/store';
 import type { AppProps } from 'next/app';
 import { useMemo } from 'react';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
 const App = ({ Component, pageProps }: AppProps): JSX.Element => {
   const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
@@ -26,7 +27,9 @@ const App = ({ Component, pageProps }: AppProps): JSX.Element => {
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <Provider store={store}>
-          <Component {...pageProps} />
+          <PersistGate persistor={persistor}>
+            <Component {...pageProps} />
+          </PersistGate>
         </Provider>
       </ThemeProvider>
     </>
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,16 +1,31 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import storage from 'redux-persist/lib/storage';
 import searchReducer from 'features/search/searchSlice';
-import forecastSlice from 'features/forecast/forecastSlice';
+import forecastReducer from 'features/forecast/forecastSlice';
+import { persistReducer, persistStore } from 'redux-persist';
+import thunk from 'redux-thunk';
+
+const reducers = combineReducers({
+  search: searchReducer,
+  forecast: forecastReducer,
+});
+
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, reducers);
 
 const store = configureStore({
-  reducer: {
-    search: searchReducer,
-    forecast: forecastSlice,
-  },
+  reducer: persistedReducer,
+  middleware: [thunk],
 });
 
 export type RootState = ReturnType<typeof store.getState>;
 
 export type AppDispatch = typeof store.dispatch;
+
+export const persistor = persistStore(store);
 
 export default store;
